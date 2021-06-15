a7c742800b77fd414d694891e052c8d51c67b7e4
stevenmldn <scatmul88@aol.com>
2021-06-15 20:05:35 +0100

    Patching the patch. This was to deal with URL which were not encoded. It may have worked for that, but seemed to break almost everything else

--- src/ios/CDVWKWebViewFileXhr.m
+++ src/ios/CDVWKWebViewFileXhr.m
@@ -335,9 +335,7 @@ NS_ASSUME_NONNULL_BEGIN
     if (urlString.length == 0) {
         return sendResult( @{ @"error" : @"Invalid url"});
     }
-    
-    urlString = [urlString stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]; // PATCH
-    
+     
     NSURL *url = [NSURL URLWithString:urlString];
     
     if (![url.scheme.lowercaseString isEqualToString:@"http"] && ![url.scheme.lowercaseString isEqualToString:@"https"]) {
